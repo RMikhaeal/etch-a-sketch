76d35295534e32074b15c811391f3768274ce560
AcousticSavant <123994190+RMikhaeal@users.noreply.github.com>
2023-06-21 14:00:07 -0400

    Add functionality to slider and grid change button

--- etch.js
+++ etch.js
@@ -7,22 +7,30 @@ function startup() {
   colorPicker = document.querySelector("#colorpicker");
   colorPicker.value = defaultColor;
   colorPicker.addEventListener("input", updateFirst, false);
+  createGrid(slider.value);
 }
 
 function updateFirst(event) {
-    colorPicker.value = event.target.value;
+  colorPicker.value = event.target.value;
+}
+
+//Slider value
+var slider = document.getElementById("slider");
+var output = document.getElementById("slidervalue");
+output.innerHTML = slider.value + " x " + slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value + " x " + this.value;
 }
 
 //Grid size selection
-let columns = 64;
-let rows = 64;
 const container = document.querySelector('.container');
 
-function createGrid(c, r) {
-  for (let i = 0; i < c; ++i) {
+function createGrid(s) {
+  for (let i = 0; i < s; ++i) {
     var col = document.createElement('div');
     col.className = 'col';
-    for (let j = 0; j < r; ++j) {
+    for (let j = 0; j < s; ++j) {
       var row = document.createElement('div');
       row.className = 'row';
       row.addEventListener('mouseover', colorDiv);
@@ -32,7 +40,14 @@ function createGrid(c, r) {
   }
 }
 
-createGrid(columns, rows);
+const button = document.getElementById("changegrid");
+
+button.addEventListener('click', () => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  createGrid(slider.value);
+});
 
 function colorDiv(e) {
   if (e.buttons == 1) {
